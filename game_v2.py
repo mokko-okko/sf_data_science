5c40e6280d2c29ed7d9ebeff045869625779095f
mokko-okko <kostrov.yk@gmail.com>
2024-02-11 17:14:40 +0300

    Adde own function for NUMBER search

--- game_v2.py
+++ game_v2.py
@@ -43,5 +43,54 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+    Returns:
+        int: Число попыток
+    Method: дихотомии, делим отрезок на двое до тех пор  пока не сойдется к числу. 
+    """
+
+    # Ваш код начинается здесь
+    
+    def div_list(a_list):
+      """
+      деление отрезка на 2 части
+      Args: a_list (int, list): Список int от 0 до 100
+      Returns: две части от начального  списка 
+      """  
+      
+      if len(a_list)>=2:
+          return a_list[0:int(len(a_list)/2)] , a_list[int(len(a_list)/2):]
+      else:
+          return a_list,a_list
+    
+    a_list=list(range(0,101))  #   формируем список чисел который далее будем разбивать на части     
+    b_part,c_part =  div_list(a_list)    
+
+    count=0 
+    while len(b_part)>=2 or len(c_part)>=2:  #цикл разбиения начального списка на части, включающие заданное число
+        count+=1
+        if number in b_part:
+            b_part,c_part =  div_list(b_part) 
+        else:    
+            b_part,c_part =  div_list(c_part)
+      
+    # if b[0]==number:
+    #     count+=1
+    #     print(b[0])
+    # else: print(c[0])
+
+    count+=1   # увеличиваем на 1 т.к. для точного ответа необходима доп проверка  какое именно число из двух списоков соответствует
+
+    # Ваш код заканчивается здесь
+
+    return count
+
+
+
 # RUN
-score_game(random_predict)
+if __name__ == '__main__':
+    score_game(game_core_v3)
